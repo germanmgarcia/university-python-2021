edb6060373b180a111c7d91a60a81766c7bbaf17
Germán <gmgarcia@ucundinamarca.edu.co>
2021-11-16 19:40:16 -0500

    Tipos de Datos en Python

---
+++ Seccion3/tipos_datos.py
@@ -0,0 +1,5 @@
+# x: str = "Hola Mundo" / True / 10 Esto no quiere decir que la variable X solamente puede almacenar tipos string.
+# Esto no es correcto, ya que si asignamos un valor de tipo entero vamos a ver que funciona sin ningún problema.
+x = True
+print(x)
+print(type(x))
