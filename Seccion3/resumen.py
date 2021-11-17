b80e2a623ea915b93b2b96eaf45503d127ca43ec
Germán <gmgarcia@ucundinamarca.edu.co>
2021-11-16 19:42:23 -0500

    Resumen Tipos de Datos en Python

---
+++ Seccion3/resumen.py
@@ -0,0 +1,19 @@
+#Tipos int
+x = 10
+print(x)
+print(type(x))
+
+#Tipo floar
+x = 14.5
+print(x)
+print(type(x))
+
+#Tipo String
+x = 'Hola Mundo'
+print(x)
+print(type(x))
+
+#Tipo bool
+x = False
+print(x)
+print(type(x))
