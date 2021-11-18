73fc8d0c777e57a4bae7a27165be5a347777c310
Germán <gmgarcia@ucundinamarca.edu.co>
2021-11-17 21:25:05 -0500

    Más Temas de Manejo de Cadenas

--- Seccion3/manejo_cadena.py
+++ Seccion3/manejo_cadena.py
@@ -1,4 +1,14 @@
-#Cadena (String)
+# Cadena (String)
 miGrupoFavorito = "Arosmith" + " "
 comentario = "The Best Rock Band"
-print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenación:", numero1 + numero2)
+print("Suma:", int(numero1) + int(numero2))
+
+# numero1 = 1
+# numero2 = 2
+# print(numero1 + numero2)
