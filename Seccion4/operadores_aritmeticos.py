a47162eb1608a29f9faa4b1802f776976ba93ba2
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-15 18:24:22 -0500

    Operadores Aritméticos en Python - parte 2

--- Seccion4/operadores_aritmeticos.py
+++ Seccion4/operadores_aritmeticos.py
@@ -3,3 +3,15 @@ operandoB = 2
 suma = operandoA + operandoB
 # print('Resultado suma: ', suma)
 print(f'Resultado suma: {suma}')
+resta = operandoA - operandoB
+print(f'Resultado resta: {resta}')
+multiplicacion = operandoA * operandoB
+print(f'Resultado de la multiplicación: {multiplicacion}')
+division = operandoA / operandoB
+print(f'Resultado división: {division}')
+division = operandoA // operandoB
+print(f'Resultado visión (int): {division}')
+modulo = operandoA % operandoB
+print(f'Resultado residuo división (módulo): {modulo}')
+exponente = operandoA ** operandoB
+print(f'Resultado exponente: {exponente}')
