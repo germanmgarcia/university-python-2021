09f0dd0a30e55262818e68f068afb88d30ad028c
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-19 18:37:15 -0500

    Diccionarios en Python

---
+++ Seccion7/diccionarios.py
@@ -0,0 +1,16 @@
+# dict (key, value)
+diccionario = {
+    'IDE': 'Integrated Development Environment',
+    'OOP': 'Object Oriented Programming',
+    'DBMS': 'Database Management System'
+}
+print(diccionario)
+# Largo
+print(len(diccionario))
+# Acceder a un elemento (key)
+print(diccionario['IDE'])
+# Otra forma de recuperar un elemento
+print(diccionario.get('OOP'))
+# Modificando elementos
+diccionario['IDE'] = 'integrated development environment'
+print(diccionario)
