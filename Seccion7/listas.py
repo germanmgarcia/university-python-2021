73721322d080bf3ee2a32abe28558d9d17bea560
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-19 13:52:01 -0500

    Listas en Python

---
+++ Seccion7/listas.py
@@ -0,0 +1,10 @@
+# Definir una lista de tipo str
+nombres = ['juan', 'karla', 'Ricardo', 'Maria']
+# imprimir la lista nombres
+print(nombres)
+# acceder a los elementos de una lista
+print(nombres[0])
+print(nombres[1])
+# acceder a los elementos de manera inversa
+print(nombres[-1])
+print(nombres[-2])
