628fd7ec2b5357a5195497de57ab83e86bcb855e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-19 18:13:23 -0500

    Tuplas en Python - parte 2

--- Seccion7/tuplas.py
+++ Seccion7/tuplas.py
@@ -8,4 +8,15 @@ print(frutas[0])
 # Navegación inversa
 print(frutas[-1])
 # Acceder a un rango
-print(frutas[ 0:1]) # Sin incluir el último índice
+print(frutas[0:1]) # Sin incluir el último índice
+# Recorrer elementos
+for fruta in frutas:
+    print(fruta, end=' ')
+# Cambiar valor tupla
+frutasLista = list(frutas)
+frutasLista[0] = 'Pera'
+frutas = tuple(frutasLista)
+print('\n', frutas)
+# Eliminar la tupla
+del frutas
+print(frutas)
