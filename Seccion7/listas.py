5571316978a3369e21bea56b636c8d9bf3b1f855
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-19 14:04:11 -0500

    Listas en Python - parte 2

--- Seccion7/listas.py
+++ Seccion7/listas.py
@@ -8,3 +8,17 @@ print(nombres[1])
 # acceder a los elementos de manera inversa
 print(nombres[-1])
 print(nombres[-2])
+# Imprimir un rango
+print(nombres[0:2]) # sin incluir el índice 2
+# Ir del inicio de la lista al índice (sin incluirlo)
+print(nombres[:3])
+# Desde el índice indicado hasta el final
+print(nombres[1:])
+# Cambiar un valor
+nombres[3] = 'Ivone'
+print(nombres)
+# Iterar una lista
+for nombre in nombres:
+    print(nombre)
+else:
+    print('No existen más nombres en la lista')
