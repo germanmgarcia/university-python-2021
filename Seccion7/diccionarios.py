6a4993d45c4165bd8eeea0c46605bfb83d959837
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-20 15:20:29 -0500

    Diccionarios en Python - parte 2

--- Seccion7/diccionarios.py
+++ Seccion7/diccionarios.py
@@ -14,3 +14,31 @@ print(diccionario.get('OOP'))
 # Modificando elementos
 diccionario['IDE'] = 'integrated development environment'
 print(diccionario)
+# Recorrer los elementos
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+for termino in diccionario.keys():
+    print(termino)
+
+for valor in diccionario.values():
+    print(valor)
+
+# Comprobar existencia de algún elemento
+print('IDE' in diccionario)
+
+# Agregar un elemento
+diccionario['PK'] = 'Primary Key'
+print(diccionario)
+
+# Remover un elemento
+diccionario.pop('DBMS')
+print(diccionario)
+
+# Limpiar
+diccionario.clear()
+print(diccionario)
+
+# Eliminar el diccionario
+del diccionario
+print(diccionario)
