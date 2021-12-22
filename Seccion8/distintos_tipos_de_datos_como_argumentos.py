e9aa75269f77d39b64cfce1f3a2fd0644cc6558f
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-21 20:02:39 -0500

    Distintos tipos de datos como argumentos en Python

---
+++ Seccion8/distintos_tipos_de_datos_como_argumentos.py
@@ -0,0 +1,9 @@
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+
+nombres = ['Juan', 'Karla', 'Guillermo']
+desplegarNombres(nombres)
+desplegarNombres('carlos')
+desplegarNombres([10, 11])
+desplegarNombres((10, 11))
