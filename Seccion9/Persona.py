168cb9c33b17cd28e0f0423650ec94451fad6b79
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-23 19:13:45 -0500

    Creación de más Objetos de un Clase

--- Seccion9/Persona.py
+++ Seccion9/Persona.py
@@ -5,6 +5,7 @@ class Persona:
         self.edad = edad
 
 persona1 = Persona('Juan', 'Perez', 28)
-print(persona1.nombre)
-print(persona1.apellido)
-print(persona1.edad)
+print(f'Objeto Persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
+
+persona2 = Persona('Karla', 'Gomez', 30)
+print(f'Objeto Persona 2: {persona2.nombre} {persona2.apellido} {persona2.edad}')
