c5a491a43eb814d29e08ff136ef87a45babe8b8e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-27 20:27:01 -0500

    Robusteciendo el Método Init

--- Seccion9/Persona.py
+++ Seccion9/Persona.py
@@ -1,18 +1,17 @@
 class Persona:
-    def __init__(self, nombre, apellido, edad):
+    def __init__(self, nombre, apellido, edad, *valores, **terminos):
         self.nombre = nombre
         self.apellido =  apellido
         self.edad = edad
+        self.valores = valores
+        self.terminos = terminos
 
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}')
 
-persona1 = Persona('Juan', 'Perez', 28)
+persona1 = Persona('Juan', 'Perez', 28, '45451231', 2 , 3, 5, m='manzana', p='pera')
 persona1.mostrar_detalle()
-persona1.telefono = '544832148'
-print(persona1.telefono)
-# Persona.mostrar_detalle(persona1)
 
 persona2 = Persona('Karla', 'Gomez', 30)
 persona2.mostrar_detalle()
-print(persona2.telefono)
+
