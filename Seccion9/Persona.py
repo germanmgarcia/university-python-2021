5b2d453398bd2b0999dcb5435ae0ebad7822804d
Germán <gmgarcia@ucundinamarca.edu.co>
2021-12-27 20:02:30 -0500

    Métodos de Instancia en Python

--- Seccion9/Persona.py
+++ Seccion9/Persona.py
@@ -4,14 +4,11 @@ class Persona:
         self.apellido =  apellido
         self.edad = edad
 
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+
 persona1 = Persona('Juan', 'Perez', 28)
-print(f'Objeto Persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
-
-persona1.nombre = 'Juan Carlos'
-persona1.apellido = 'Juarez'
-persona1.edad = 25
-
-print(f'Objeto Persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
+persona1.mostrar_detalle()
 
 persona2 = Persona('Karla', 'Gomez', 30)
-print(f'Objeto Persona 2: {persona2.nombre} {persona2.apellido} {persona2.edad}')
+persona2.mostrar_detalle()
