b13037d570d57db91489908761231b7d8b8e986e
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-01 17:44:59 -0500

    Validaciones Aplicación de Figura Geométrica - parte 2

--- Seccion12/FiguraGeometrica.py
+++ Seccion12/FiguraGeometrica.py
@@ -1,13 +1,15 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        if 0 < ancho < 10:
+        if self._validar_valor(ancho):
             self._ancho = ancho
         else:
             self._ancho = 0
-        if 0 < alto < 10:
+            print(f'Valor erroneo ancho: {ancho}')
+        if self._validar_valor(alto):
             self._alto = alto
         else:
             self._alto = 0
+            print(f'Valor erroneo alto: {alto}')
         
     @property
     def ancho(self):
@@ -15,7 +17,10 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor erroneo ancho: {ancho}')
 
     @property
     def alto(self):
@@ -23,7 +28,13 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            print(f'Valor erroneo ancho: {alto}')
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
+
+    def _validar_valor(self, valor):
+        return True if 0 < valor < 10 else False
--- Seccion12/test_figura_geometrica.py
+++ Seccion12/test_figura_geometrica.py
@@ -3,10 +3,12 @@ from Rectangulo import Rectangulo
 
 print('Creación Objeto Cuadrado'.center(50, '-'))
 cuadrado1 = Cuadrado(lado=5, color='rojo')
+cuadrado1.alto = -10
 print(f'Cálculo área cuadrado: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
 print('Creación Objeto Rectángulo'.center(50, '-'))
-rectangulo1 = Rectangulo(ancho=3, alto=8, color='verde')
+rectangulo1 = Rectangulo(ancho=9, alto=8, color='verde')
+rectangulo1.ancho = 15
 print(f'Cálculo área rectángulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
