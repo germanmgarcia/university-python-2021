c1c99c7d94ac669aabd85532fb17184e73873335
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-01 17:39:31 -0500

    Validaciones Aplicación de Figura Geométrica

--- Seccion12/test_figura_geometrica.py
+++ Seccion12/test_figura_geometrica.py
@@ -1,10 +1,12 @@
 from Cuadrado import  Cuadrado
 from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'rojo')
+print('Creación Objeto Cuadrado'.center(50, '-'))
+cuadrado1 = Cuadrado(lado=5, color='rojo')
 print(f'Cálculo área cuadrado: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
-rectangulo1 = Rectangulo(3, 8, 'verde')
+print('Creación Objeto Rectángulo'.center(50, '-'))
+rectangulo1 = Rectangulo(ancho=3, alto=8, color='verde')
 print(f'Cálculo área rectángulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
