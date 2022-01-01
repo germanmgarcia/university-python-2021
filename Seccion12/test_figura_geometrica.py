04748c512400dedba3930cc5c296dea2b2f72093
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-01 17:35:38 -0500

    Solución Laboratorio Figura Geométrica

--- Seccion12/test_figura_geometrica.py
+++ Seccion12/test_figura_geometrica.py
@@ -1,10 +1,10 @@
 from Cuadrado import  Cuadrado
+from Rectangulo import Rectangulo
 
 cuadrado1 = Cuadrado(5, 'rojo')
-# print(cuadrado1.ancho)
-# print(cuadrado1.alto)
-# print(cuadrado1.color)
 print(f'Cálculo área cuadrado: {cuadrado1.calcular_area()}')
+print(cuadrado1)
 
-#MRO - Method Resolution Order
-print(Cuadrado.mro())
+rectangulo1 = Rectangulo(3, 8, 'verde')
+print(f'Cálculo área rectángulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
