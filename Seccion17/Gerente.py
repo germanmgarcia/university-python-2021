1fb7d4ec53f5b3d37ef61640194ad47797db2e6d
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-13 22:41:53 -0500

    Polimorfismo en Python - parte 2

--- Seccion17/Gerente.py
+++ Seccion17/Gerente.py
@@ -7,4 +7,7 @@ class Gerente(Empleado):
         self.departamento = departamento
 
     def __str__(self):
-        return f'Gerente: [Departamento: {self.departamento} {super().__str__()}]'
+        return f'Gerente: [Departamento: {self.departamento} ] {super().__str__()}'
+
+    # def mostrar_detalles(self):
+    #     return self.__str__()
