165ab4cf2377a6b8e960a018c153db6c4217b490
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-17 20:27:53 -0500

    Procesamiento de Excepciones en Python

--- Seccion19/manejo_exceptiones.py
+++ Seccion19/manejo_exceptiones.py
@@ -1,4 +1,11 @@
+resultado = None
+a = '10'
+b = 0
+
 try:
-    10/0
+    resultado = a/b
 except Exception as e:
     print(f'Ocurrió un error: {e}')
+
+print(f'Resultado: {resultado}')
+print('Continuamos...')
