7a1beafb89abc09ed74f7fd58b0188a6b1551571
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-17 20:52:45 -0500

    Lectura de Archivos en Python

---
+++ Seccion20/leer_archivo.py
@@ -0,0 +1,11 @@
+# archivo = open('G:\\Proyectos-Programación\\university-python-2021\\Seccion20\\prueba.txt', 'r', encoding='utf8')
+archivo = open('prueba.txt', 'r', encoding='utf8')
+# print(archivo.read())
+
+#leer algunos caracteres
+# print(archivo.read(5))
+# print(archivo.read(3))
+
+#leer lineas completas
+print(archivo.readline())
+print(archivo.readline())
