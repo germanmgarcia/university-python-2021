598896d01694331e22d384ec6fb647b909d585b2
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-17 21:07:33 -0500

    Uso de with y Context Manager con Python

--- Seccion21/archivos_con_with.py
+++ Seccion21/archivos_con_with.py
@@ -1,2 +1,5 @@
-with open('prueba.txt', 'r', encoding='utf8') as archivo:
+# with open('prueba.txt', 'r', encoding='utf8') as archivo:
+from ManejoArchivos import ManejoArchivos
+
+with ManejoArchivos('prueba.txt') as archivo:
     print(archivo.read())
