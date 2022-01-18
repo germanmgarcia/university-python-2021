4533b506548a26d26420556980aaf20f24153dd4
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-17 20:47:36 -0500

    Especificar el Juego de Caracteres de un Archivo de Texto

--- Seccion20/manejo_archivos.py
+++ Seccion20/manejo_archivos.py
@@ -1,8 +1,10 @@
 try:
-    archivo = open('prueba.txt', 'w')
-    archivo.write('Agregamos informacion al archivo\n')
+    archivo = open('prueba.txt', 'w', encoding='utf8')
+    archivo.write('Agregamos información al archivo\n')
     archivo.write('Adios')
 except Exception as e:
     print(e)
 finally:
     archivo.close()
+    print('Fin del archivo')
+    # archivo.write('nueva info')
