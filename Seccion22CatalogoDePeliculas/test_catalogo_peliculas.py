20d8fee2952aa12b5600c3a925a2f75b166cd9ae
Germán <gmgarcia@ucundinamarca.edu.co>
2022-01-24 20:47:20 -0500

    Test Catálogo de Películas - parte 2

--- Seccion22CatalogoDePeliculas/test_catalogo_peliculas.py
+++ Seccion22CatalogoDePeliculas/test_catalogo_peliculas.py
@@ -1,3 +1,6 @@
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
 opcion = None
 while opcion != 4:
     try:
@@ -7,6 +10,14 @@ while opcion != 4:
         print('3. Eliminar catálogo películas')
         print('4. Salir')
         opcion = int(input('Escribe tu opción (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Proporciona el nombre de la película: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_pelicula(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
     except Exception as e:
         print(f'Ocurrió un error {e}')
         opcion = None
