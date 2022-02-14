0b4a11b912d82f24fdeb3acec282eb942a4401f0
German Garcia <gmgarcia@ucundinamarca.edu.co>
2022-02-14 07:50:16 -0800

    Creación de otra clase de Modelo

--- Seccion65Django/personas/models.py
+++ Seccion65Django/personas/models.py
@@ -1,12 +1,19 @@
 from importlib.util import module_for_loader
 from django.db import models
 
-# Create your models here.
+class Domicilio(models.Model):
+    calle = models.CharField(max_length=255)
+    no_calle = models.IntegerField()
+    pais = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'Domicilio {self,id} {self.calle} {self.no_calle} {self.pais}'
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+    domicilio = models.ForeignKey(Domicilio, on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         return f'Persona {self.id} {self.nombre} {self.apellido} {self.email}'
