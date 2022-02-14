3d33f37231d7f63159448ebd11b251f76087263e
German Garcia <gmgarcia@ucundinamarca.edu.co>
2022-02-14 12:46:03 -0800

    Eliminar un Objeto de Modelo en Django

--- Seccion65Django/personas/views.py
+++ Seccion65Django/personas/views.py
@@ -25,8 +25,14 @@ def editarPersona(request, id):
     if request.method == 'POST':
         formaPersona = PersonaForm(request.POST, instance=persona)
         if formaPersona.is_valid():
-            formaPersona.save()
+            formaPersona.save() 
             return redirect('inicio')
     else:        
         formaPersona = PersonaForm(instance=persona)
     return render(request, 'personas/editar.html', {'formaPersona': formaPersona})
+
+def eliminarPersona(request, id):
+    persona = get_object_or_404(Persona, pk=id)
+    if persona:
+        persona.delete()
+    return redirect('inicio')
