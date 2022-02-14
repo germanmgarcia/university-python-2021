8cd498eacb6fca7e180776114c832a96058354ef
German Garcia <gmgarcia@ucundinamarca.edu.co>
2022-02-14 08:40:21 -0800

    Detalle de un Objeto de Modelo con Django

--- Seccion65Django/sap/urls.py
+++ Seccion65Django/sap/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from webapp.views import bienvenido
+from personas.views import detallePersona
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bienvenido),
+    path('detalle_persona/<int:id>', detallePersona)
 ]
