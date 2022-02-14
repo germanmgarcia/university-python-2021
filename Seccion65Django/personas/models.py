b6e3675adf582d9fb3edd61185a8108755f7f71d
German Garcia <gmgarcia@ucundinamarca.edu.co>
2022-02-14 07:42:45 -0800

    Clase de Modelo desde la consola de Administración

--- Seccion65Django/personas/models.py
+++ Seccion65Django/personas/models.py
@@ -7,3 +7,6 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'Persona {self.id} {self.nombre} {self.apellido} {self.email}'
